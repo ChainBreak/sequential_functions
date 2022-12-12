9954b0556412ad7c97cb442ea0fbb0d49c51786a
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2022-12-12 23:00:29 +1030

    Add batch support

--- src/sequential_functions/containers.py
+++ src/sequential_functions/containers.py
@@ -8,8 +8,7 @@ class Compose():
     def __call__(self, generator):
 
         for function in self.function_list:
-            
-            if type(function) is type(self):
+            if isinstance(function, Compose):
                 generator = function(generator)
             else:
                 generator = self.wrap_function_in_generator(function,generator)
@@ -19,6 +18,38 @@ class Compose():
     def wrap_function_in_generator(self,function, generator):
         for item in generator:
             yield function(item)
+
+        
+class Batch(Compose):
+    def __init__(self,*functions, batch_size):
+        super().__init__(*functions)
+
+        self.batch_size = batch_size
+
+    def __call__(self,generator):
+
+        batch_generator = self.create_batch_generator(generator)
+
+        batch_generator = super().__call__(batch_generator)
+
+        generator = self.create_debatch_generator(batch_generator)
+
+        return generator
+
+    def create_batch_generator(self,generator):
+        batch = []
+        for item in generator:
+            batch.append(item)
+            if len(batch) >= self.batch_size:
+                yield batch
+                batch = []
+        if len(batch) > 0:        
+            yield batch
+
+    def create_debatch_generator(self,generator):
+        for batch in generator:
+            yield from batch
+
 
 
 class MultiProcessSequence():
@@ -33,37 +64,5 @@ class MultiProcessSequence():
         for f in self.function_list:
             x = f(x)
         return x
-        
-class Batch():
-    def __init__(self,*callables):
-        self.function_list = callables
-        self.batch_size = 3
 
-    def __call__(self,g):
-        batch_g = self.create_batch_generator(g)
-
-        for f in self.function_list:
-            batch_g = self.wrap_function_in_generator(f,batch_g)
-
-        g = self.create_debatch_generator(batch_g)
-        return g
-
-    def create_batch_generator(self,g):
-        batch = []
-        for item in g:
-            batch.append(item)
-            if len(batch) >= self.batch_size:
-                yield batch
-                batch = []
-        yield batch
-
-    def create_debatch_generator(self,g):
-        for batch in g:
-            yield from batch
-
-    
-    def wrap_function_in_generator(self,f, g):
-        for x in g:
-            yield f(x)
-
-special_callable_types = [type(Compose), type(MultiProcessSequence), type(Batch)]
+special_callable_types = (type(Compose), type(MultiProcessSequence), type(Batch))
--- tests/test_containers.py
+++ tests/test_containers.py
@@ -1,12 +1,8 @@
 import pytest
-from sequential_functions import Compose
+from sequential_functions import Compose, Batch
 
-def double(x):
-    return 2 * x
 
-def sub_1(x):
-    return x - 1
-
+  
 
 def test_compose_build_generator_chain():
 
@@ -39,3 +35,42 @@ def test_nested_compose():
     y = [sub_1(sub_1(double(double(x)))) for x in range(n)]
     assert x==y
 
+def test_batch():
+    f = Batch(
+        assert_batch_double,
+        batch_size=2,
+    )
+
+    n = 10
+
+    x = list(f(range(n)))
+    y = [double(x) for x in range(n)]
+    assert x==y
+
+def test_batch_nested():
+    f = Compose(
+        Batch(
+            Compose(
+                assert_batch_double,
+            ),
+            batch_size=2,
+        ),
+    )
+
+    n = 10
+
+    x = list(f(range(n)))
+    y = [double(x) for x in range(n)]
+    assert x==y
+
+def double(x):
+    return 2 * x
+
+def sub_1(x):
+    return x - 1
+
+def assert_batch_double(x_batch):
+    assert type(x_batch) is list
+    assert len(x_batch) > 0
+    y_batch = [double(x) for x in x_batch]
+    return y_batch
